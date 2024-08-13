5e3e924bda2a7ee2b7eaefd584a259b5d5938b46
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-08-13 09:17:03 +0200

    Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import styles from "./Loader.module.css";
+import { ThreeDots } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <ThreeDots
+      color="#465df3"
+      height={80}
+      width={80}
+      wrapperClass={styles.loader}
+    />
+  );
+};
+
+export default Loader;
